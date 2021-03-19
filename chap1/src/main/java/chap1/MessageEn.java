dc7dd88297b791f8b5095b0f9b16c87c9785e54f
hyunsik-Nam <79626331+hyunsik-Nam@users.noreply.github.com>
2021-03-19 12:01:06 +0900

    Delete chap1 directory

--- chap1/src/main/java/chap1/MessageEn.java
+++
@@ -1,7 +0,0 @@
-package chap1;
-
-public class MessageEn implements Message{
-	public void sayHello(String msg) {
-		System.out.println("hello"+msg);
-	}
-}
--- chap1/src/main/java/chap1/MessageKr.java
+++
@@ -1,7 +0,0 @@
-package chap1;
-
-public class MessageKr implements Message {
-	public void sayHello(String msg) {
-		System.out.println("반갑습니다."+msg+"님");
-	}
-}
